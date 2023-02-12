7f200d3f82bfd69f43deb767a900ce2522c864a3
zorua98741 <83170597+zorua98741@users.noreply.github.com>
2023-02-12 18:00:16 +1100

    added files for github pages

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# SCE-tmdb-scrape
+# SCE-tmdb
+This repository contains a scrape of a majority of possible titleIDs, as well as a webpage which can be used to generate tmdb URLs.
  
---
+++ webpage/tmdb.js
@@ -0,0 +1,34 @@
+function generateHash() {
+	const shaObj = new jsSHA("SHA-1", "TEXT", {
+		hmacKey: {value: "F5DE66D2680E255B2DF79E74F890EBF349262F618BCAE2A9ACCDEE5156CE8DF2CDF2D48C71173CDC2594465B87405D197CF1AED3B7E9671EEB56CA6753C2E6B0", format: "HEX"},
+	});
+	var titleID = document.getElementById("titleIDText");
+	
+	let x = titleID.value;
+	let y = "_00";
+	let input = x.concat(y);
+	let inputUpper = input.toUpperCase();
+	
+	shaObj.update(inputUpper);
+	let hmac = shaObj.getHash("HEX");
+	let output = hmac.toUpperCase();
+	document.getElementById("output").innerHTML = output;
+	document.getElementById("input").innerHTML = inputUpper;
+}
+function generateURL() {
+	var pType = document.getElementById("platform").value;
+	if (pType == "PS4 (JSON)") {
+		var tmdb = "http://tmdb.np.dl.playstation.net/tmdb2/"
+		var extension = ".json"
+	} else {
+		var tmdb = "http://tmdb.np.dl.playstation.net/tmdb/"
+		var extension = ".xml"
+	}
+	var titleID = document.getElementById("input").textContent;
+	if (titleID != "___") { 	//stop incomplete link from being generated and shown
+		var hash = document.getElementById("output").textContent;
+		var genURL = tmdb + titleID + "_" + hash + "/" + titleID + extension;
+		document.getElementById("url").innerHTML = genURL;
+		document.getElementById("url").href = genURL;
+	}
+}
